5883ad7f3b78ec58a1a373e77bf5d8377e95a006
EricFerndz <eric.ferndz@icloud.com>
2026-05-08 23:15:01 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "contrat-cache-v1"
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./logo.png",
+  "./signature.png",
+  "./html2pdf.bundle.min.js"
+]
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  )
+
+})
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  )
+
+})
